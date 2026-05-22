3120798fd1ad4f8058d3a87361ae72b9053bb4f8
manerichmaria406-a11y <manerichmaria406@gmail.com>
2026-05-22 19:56:14 -0300

    Projeto Git

---
+++ cadastro.js
@@ -0,0 +1,15 @@
+let codigo = 50
+
+switch(codigo){
+    case 50 :
+        console.log("banana");
+        break;
+
+    case 9540 :
+        console.log("limão");
+        break;
+        
+    default :
+    console.log("desconhecido");
+    break;    
+}
---
+++ gt.js
@@ -0,0 +1,4 @@
+let i = 5;
+for(; i < 10 ; i ++){
+    console.log(i);
+}
